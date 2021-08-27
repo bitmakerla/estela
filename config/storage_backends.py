d791605ec4d4b1c7f868e0186863801b2b196380
Alonso Gonzalo Valdivia Quispe <alonso.valdivia.quispe@ucsp.edu.pe>
2021-08-27 02:13:37 +0000

    BITMAKER-1030: Fix static path in API

--- config/storage_backends.py
+++ config/storage_backends.py
@@ -1,11 +1,11 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-from config.settings import base
+from config.settings import prod
 
 
 class MediaStorage(S3Boto3Storage):
-    location = base.MEDIAFILES_LOCATION
+    location = prod.MEDIAFILES_LOCATION
     file_overwrite = False
 
 
 class StaticStorage(S3Boto3Storage):
-    location = base.STATICFILES_LOCATION
+    location = prod.STATICFILES_LOCATION
