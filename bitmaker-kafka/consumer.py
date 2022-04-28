4e340b5876be7259c7220508723204dfe4d20e0c
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2022-04-28 02:56:02 -0500

    Encode/decode dots in json keys for mongoDB

--- bitmaker-kafka/consumer.py
+++ bitmaker-kafka/consumer.py
@@ -44,9 +44,26 @@ def connect_kafka_consumer(topic_name):
         return _consumer
 
 
+# https://stackoverflow.com/questions/12397118/mongodb-dot-in-key-name
+def mongo_jsonify(item):
+    new_item = {}
+    if type(item) is dict:
+        for key, value in item.items():
+            new_key = key.replace(".", "\\u002e")
+            if type(value) is dict:
+                new_item[new_key] = mongo_jsonify(value)
+            elif type(value) is list:
+                new_item[new_key] = [mongo_jsonify(x) for x in value]
+            else:
+                new_item[new_key] = item[key]
+        return new_item
+    else:
+        return item
+
+
 def read_from_queue(client):
     while True:
-        item = item_queue.get()
+        item = mongo_jsonify(item_queue.get())
         try:
             if item["unique"] == "True":
                 client[item["database_name"]][item["collection_name"]].update_one(
