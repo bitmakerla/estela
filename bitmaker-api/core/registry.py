34655f1e227ee8cad1eec8b3a51365e06a54dcb2
Alonso-Valdivia <34718875+Adriana618@users.noreply.github.com>
2022-06-06 18:52:50 -0500

    BITMAKER-1618: Add local S3 (#23)
    * BITMAKER-1618: Add local S3
    
    * BITMAKER-1618: Removed bitmaker project_storage, removed print in build.py and fix WORKDIR in build project
    
    * BITMAKER-1618: Changed name of credentials in local, linted code and changed volume of minio

--- bitmaker-api/core/registry.py
+++ bitmaker-api/core/registry.py
@@ -4,15 +4,6 @@ from elasticsearch import Elasticsearch, RequestsHttpConnection
 from django.conf import settings
 
 
-def upload_project_to_s3(project_name, project_obj):
-    s3_client = boto3.client("s3")
-    try:
-        s3_client.upload_fileobj(project_obj, settings.PROJECT_BUCKET, project_name)
-    except ClientError as e:
-        return e
-    return False
-
-
 def get_logs(spiderjob, offset, page_size):
     es = Elasticsearch(
         hosts=[
