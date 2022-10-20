49851dcbde19a09dd5bfc5e524dde0e3926551b1
Raymond Negron Valqui <33165861+raymond1242@users.noreply.github.com>
2022-10-20 18:28:01 -0500

    BITMAKER-2081 estela-web: "In Project/ Members" mockup (#88)
    * Porject Side nav corrected & and first style
    
    * Modal to add user
    
    * Fix items keys warning
    
    Co-authored-by: Raymond Negron <raymond1242@Raymonds-MacBook-Air.local>

--- estela-web/src/components/ProjectMemberPage/index.tsx
+++ estela-web/src/components/ProjectMemberPage/index.tsx
@@ -1,9 +1,10 @@
-import React, { Component, Fragment } from "react";
-import { Button, Layout, Typography, Space, Descriptions, Row, Input, Select } from "antd";
+import React, { Component, Fragment, ReactElement } from "react";
+import { Button, Layout, Space, Table, Row, Col, Tag, Modal, Input, Select } from "antd";
 import { RouteComponentProps } from "react-router-dom";
 
 import "./styles.scss";
 import { ApiService, AuthService } from "../../services";
+import { ReactComponent as User } from "../../assets/icons/user.svg";
 import {
     ApiProjectsReadRequest,
     Project,
@@ -24,15 +25,23 @@ import { Permission } from "../../services/api/generated-api/models/Permission";
 import { handleInvalidDataError } from "../../utils";
 
 const { Content } = Layout;
-const { Text, Title } = Typography;
 const { Option } = Select;
+
+interface MemberState {
+    username: string | undefined;
+    email: string | undefined;
+    role: string | undefined;
+    status: string | undefined;
+}
 
 interface ProjectMemberPageState {
     name: string;
     user: string;
     users: Permission[];
     loaded: boolean;
+    modal: boolean;
     newUser: string;
+    members: MemberState[];
     permission: ProjectUpdatePermissionEnum;
 }
 
@@ -44,13 +53,43 @@ export class ProjectMemberPage extends Component<RouteComponentProps<RouteParams
     state: ProjectMemberPageState = {
         name: "",
         user: "",
+        modal: false,
         users: [],
         loaded: false,
         newUser: "",
         permission: ProjectUpdatePermissionEnum.Viewer,
+        members: [],
     };
     apiService = ApiService();
     projectId: string = this.props.match.params.projectId;
+
+    columns = [
+        {
+            title: "USERNAME",
+            dataIndex: "username",
+            key: "username",
+        },
+        {
+            title: "EMAIL",
+            dataIndex: "email",
+            key: "email",
+        },
+        {
+            title: "ROLE",
+            dataIndex: "role",
+            key: "role",
+        },
+        {
+            title: "STATUS",
+            dataIndex: "status",
+            key: "status",
+            render: (status: string): ReactElement => (
+                <Tag className="text-estela border-0 rounded bg-button-hover" key={1}>
+                    {status}
+                </Tag>
+            ),
+        },
+    ];
 
     updateInfo = (): void => {
         const requestParams: ApiProjectsReadRequest = { pid: this.projectId };
@@ -60,7 +99,16 @@ export class ProjectMemberPage extends Component<RouteComponentProps<RouteParams
                 if (users === undefined) {
                     users = [];
                 }
-                this.setState({ name: response.name, users: users, loaded: true });
+                const membersList: MemberState[] = [];
+                users.map((user: Permission) => {
+                    membersList.push({
+                        username: user.user?.username,
+                        email: user.user?.email,
+                        role: user.permission,
+                        status: "In Project",
+                    });
+                });
+                this.setState({ name: response.name, users: users, members: membersList, loaded: true });
             },
             (error: unknown) => {
                 console.error(error);
@@ -149,60 +197,114 @@ export class ProjectMemberPage extends Component<RouteComponentProps<RouteParams
     };
 
     render(): JSX.Element {
-        const { loaded, name, users, newUser } = this.state;
+        const { loaded, members, newUser } = this.state;
         return (
-            <Layout className="general-container">
+            <Layout className="">
                 <Header />
-                <Layout className="white-background">
+                <Layout className="bg-white">
                     {loaded ? (
                         <Fragment>
-                            <ProjectSidenav projectId={this.projectId} />
-                            <Content className="content-padding">
-                                <Title level={3} className="text-center">
-                                    {name}
-                                </Title>
-                                <Row justify="center" className="project-data">
-                                    <Text>
-                                        <b>Project ID:</b>&nbsp; {this.projectId}
-                                    </Text>
-                                </Row>
-                                <Row>
-                                    <Space direction="vertical">
-                                        <b>USERS</b>
-                                        {users.map((users: Permission, id) => (
-                                            <Descriptions key={id}>
-                                                <Descriptions.Item label="UserName">
-                                                    {users.user?.username}
-                                                </Descriptions.Item>
-                                                <Descriptions.Item label="Email">{users.user?.email}</Descriptions.Item>
-                                                <Descriptions.Item label="Permission">
-                                                    {users.permission}
-                                                </Descriptions.Item>
-                                            </Descriptions>
-                                        ))}
-                                        <Input
-                                            name="newUser"
-                                            placeholder="email"
-                                            value={newUser}
-                                            onChange={this.handleInputChange}
-                                        />
-                                        <Select
-                                            defaultValue={ProjectUpdatePermissionEnum.Viewer}
-                                            style={{ width: 120 }}
-                                            onChange={this.handleSelectChange}
-                                        >
-                                            <Option value={ProjectUpdatePermissionEnum.Admin}>Admin</Option>
-                                            <Option value={ProjectUpdatePermissionEnum.Developer}>Developer</Option>
-                                            <Option value={ProjectUpdatePermissionEnum.Viewer}>Viewer</Option>
-                                        </Select>
-                                    </Space>
-                                    <Button className="job-create-button" onClick={this.addUser}>
-                                        Add User
-                                    </Button>
-                                    <Button className="job-create-button" onClick={this.removeUser}>
-                                        Remove User
-                                    </Button>
-                                </Row>
+                            <ProjectSidenav projectId={this.projectId} path={"/members"} />
+                            <Content className="bg-metal rounded-2xl h-screen">
+                                <div className="lg:m-10 md:mx-6 mx-2">
+                                    <Row className="flow-root my-6">
+                                        <Col className="float-left">
+                                            <p className="text-xl font-medium text-silver float-left">
+                                                Project Members
+                                            </p>
+                                        </Col>
+                                        <Col className="float-right">
+                                            <Button
+                                                icon={<User className="mr-2" width={19} />}
+                                                size="large"
+                                                className="flex items-center stroke-white hover:stroke-estela bg-estela text-white hover:text-estela text-sm hover:border-estela rounded-md"
+                                                onClick={() => this.setState({ modal: true })}
+                                            >
+                                                Add new member
+                                            </Button>
+                                            <Modal
+                                                className=""
+                                                visible={this.state.modal}
+                                                title={<p className="text-center m-2">ADD NEW MEMBER</p>}
+                                                onCancel={() => this.setState({ modal: false })}
+                                                footer={[<></>]}
+                                            >
+                                                <div className="lg:mx-8 md:mx-8">
+                                                    <p className="lg:pb-4 py-2">Email</p>
+                                                    <Input
+                                                        className="border-estela rounded"
+                                                        name="newUser"
+                                                        placeholder="Please input your email"
+                                                        value={newUser}
+                                                        onChange={this.handleInputChange}
+                                                    />
+                                                    <p className="lg:py-4 py-2">Role</p>
+                                                    <Select
+                                                        className="w-full"
+                                                        defaultValue={ProjectUpdatePermissionEnum.Viewer}
+                                                        onChange={this.handleSelectChange}
+                                                    >
+                                                        <Option
+                                                            className="hover:bg-button-hover hover:text-estela"
+                                                            value={ProjectUpdatePermissionEnum.Admin}
+                                                        >
+                                                            Admin
+                                                        </Option>
+                                                        <Option
+                                                            className="hover:bg-button-hover hover:text-estela"
+                                                            value={ProjectUpdatePermissionEnum.Developer}
+                                                        >
+                                                            Developer
+                                                        </Option>
+                                                        <Option
+                                                            className="hover:bg-button-hover hover:text-estela"
+                                                            value={ProjectUpdatePermissionEnum.Viewer}
+                                                        >
+                                                            Viewer
+                                                        </Option>
+                                                    </Select>
+                                                    <Row className="mt-6 w-full grid grid-cols-2" justify="center">
+                                                        <Button
+                                                            size="large"
+                                                            className="mr-2 sm:mr-1 bg-estela text-white border-estela hover:text-estela hover:border-estela rounded"
+                                                            onClick={() => {
+                                                                this.setState({ modal: false });
+                                                                this.addUser();
+                                                            }}
+                                                        >
+                                                            Add
+                                                        </Button>
+                                                        <Button
+                                                            size="large"
+                                                            className="ml-2 sm:ml-1 border-estela hover:border-estela text-estela hover:text-estela"
+                                                            onClick={() => this.setState({ modal: false })}
+                                                        >
+                                                            Cancel
+                                                        </Button>
+                                                    </Row>
+                                                </div>
+                                            </Modal>
+                                        </Col>
+                                    </Row>
+                                    <Row className="bg-white rounded-lg">
+                                        <div className="m-4">
+                                            <Space direction="vertical" className="">
+                                                <p className="text-silver text-lg font-medium">Members</p>
+                                                <Table
+                                                    tableLayout="fixed"
+                                                    className="rounded-2xl"
+                                                    rowSelection={{
+                                                        type: "checkbox",
+                                                    }}
+                                                    columns={this.columns}
+                                                    dataSource={members}
+                                                    pagination={false}
+                                                    size="middle"
+                                                />
+                                            </Space>
+                                        </div>
+                                    </Row>
+                                </div>
                             </Content>
                         </Fragment>
                     ) : (
