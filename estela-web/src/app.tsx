035c464eea9b2b3613eeb7568accfe3f727d7ff9
Jose Armando <josearmando07c@gmail.com>
2022-10-30 21:58:01 -0500

    Fix problems with header

--- estela-web/src/app.tsx
+++ estela-web/src/app.tsx
@@ -2,13 +2,31 @@ import React, { Fragment, Component } from "react";
 import { Router } from "react-router-dom";
 import { MainRoutes } from "./routes";
 import { Header } from "./shared";
+import { AuthService } from "./services";
+import { authNotification } from "./shared";
 import history from "./history";
-export class App extends Component<unknown, unknown> {
+
+interface AppState {
+    loaded: boolean;
+}
+
+export class App extends Component<unknown, AppState> {
+    state: AppState = {
+        loaded: false,
+    };
+    componentDidMount(): void {
+        if (!AuthService.getAuthToken()) {
+            authNotification();
+        } else {
+            this.setState({ loaded: true });
+        }
+    }
     render(): JSX.Element {
+        const { loaded } = this.state;
         return (
             <Fragment>
                 <Router history={history}>
-                    <Header />
+                    {loaded ? <Header /> : <Fragment></Fragment>}
                     <MainRoutes />
                 </Router>
             </Fragment>
