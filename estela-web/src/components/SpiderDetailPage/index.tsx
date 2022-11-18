c0ec834db497ad89654cc2b1329c6385edd42a9a
saul <luckly083@gmail.com>
2022-11-18 13:03:29 -0500

    BITMAKER-2163: Add [Head Section] and [Menu Tab] of the Spider Detail Page

--- estela-web/src/components/SpiderDetailPage/index.tsx
+++ estela-web/src/components/SpiderDetailPage/index.tsx
@@ -1,5 +1,7 @@
 import React, { Component, ReactElement } from "react";
-import { Button, Layout, Pagination, Typography, Row, Space, Table } from "antd";
+import { Button, Layout, Pagination, Typography, Row, Space, Table, Col, Menu } from "antd";
+import type { MenuProps } from "antd";
+import { PlusOutlined, PlayCircleOutlined } from "@ant-design/icons";
 import { RouteComponentProps, Link } from "react-router-dom";
 
 import "./styles.scss";
@@ -30,6 +32,7 @@ interface SpiderDetailPageState {
     loaded: boolean;
     count: number;
     current: number;
+    pageTab: string;
 }
 
 interface RouteParams {
@@ -45,6 +48,7 @@ export class SpiderDetailPage extends Component<RouteComponentProps<RouteParams>
         loaded: false,
         count: 0,
         current: 0,
+        pageTab: "overview",
     };
     apiService = ApiService();
     projectId: string = this.props.match.params.projectId;
@@ -83,6 +87,17 @@ export class SpiderDetailPage extends Component<RouteComponentProps<RouteParams>
         },
     ];
 
+    itemsDetailMenu: MenuProps["items"] = [
+        {
+            label: <Text className="text-[#6C757D]">Overview</Text>,
+            key: "overview",
+        },
+        {
+            label: <Text className="text-[#6C757D]">Settings</Text>,
+            key: "settings",
+        },
+    ];
+
     async componentDidMount(): Promise<void> {
         if (!AuthService.getAuthToken()) {
             authNotification();
@@ -107,6 +122,12 @@ export class SpiderDetailPage extends Component<RouteComponentProps<RouteParams>
             );
         }
     }
+
+    onDetailMenuTabChange: MenuProps["onClick"] = (e) => {
+        this.setState({
+            pageTab: e.key,
+        });
+    };
 
     getSpiderJobs = async (page: number): Promise<{ data: SpiderJobData[]; count: number; current: number }> => {
         const requestParams: ApiProjectsSpidersJobsListRequest = {
@@ -140,7 +161,7 @@ export class SpiderDetailPage extends Component<RouteComponentProps<RouteParams>
     };
 
     render(): JSX.Element {
-        const { loaded, name, jobs, count, current } = this.state;
+        const { loaded, name, jobs, count, current, pageTab } = this.state;
         return (
             <Layout className="general-container">
                 <Header />
@@ -150,41 +171,84 @@ export class SpiderDetailPage extends Component<RouteComponentProps<RouteParams>
                         {loaded ? (
                             <Layout className="white-background">
                                 <Content>
-                                    <Title level={4} className="text-center">
-                                        {name}
-                                    </Title>
-                                    <Row justify="center" className="spider-data">
-                                        <Space direction="vertical" size="large">
-                                            <Text>
-                                                <b>Spider ID:</b>&nbsp; {this.spiderId}
-                                            </Text>
-                                            <Text>
-                                                <b>Project ID:</b>
-                                                <Link to={`/projects/${this.projectId}`}>&nbsp; {this.projectId}</Link>
-                                            </Text>
-                                            <Table
-                                                columns={this.columns}
-                                                dataSource={jobs}
-                                                pagination={false}
+                                    <Row>
+                                        <Col span={4}>
+                                            <Text className="text-[#6C757D]">My Spider</Text>
+                                        </Col>
+                                        <Col span={4} offset={11}>
+                                            <Button
+                                                className="
+                                                hover:bg-indigo-600 hover:text-white 
+                                                text-white bg-indigo-600 rounded-md
+                                                py-5 alignment-button-icon"
+                                                icon={<PlusOutlined />}
                                                 size="middle"
-                                            />
-                                        </Space>
+                                            >
+                                                <Text className="text-white">Schedule new job</Text>
+                                            </Button>
+                                        </Col>
+                                        <Col span={4} offset={1}>
+                                            <Button
+                                                className="                                                hover:bg-indigo-600 hover:text-white 
+                                                text-white bg-indigo-600 rounded-md
+                                                py-5 alignment-button-icon"
+                                                icon={<PlayCircleOutlined />}
+                                                size="middle"
+                                            >
+                                                <Text className="text-white">Run new job</Text>
+                                            </Button>
+                                        </Col>
                                     </Row>
-                                    <Pagination
-                                        className="pagination"
-                                        defaultCurrent={1}
-                                        total={count}
-                                        current={current}
-                                        pageSize={this.PAGE_SIZE}
-                                        onChange={this.onPageChange}
-                                        showSizeChanger={false}
+                                    <Menu
+                                        onClick={this.onDetailMenuTabChange}
+                                        selectedKeys={[this.state.pageTab]}
+                                        mode="horizontal"
+                                        items={this.itemsDetailMenu}
                                     />
-                                    <Link to={`/projects/${this.projectId}/spiders/${this.spiderId}/jobs/create`}>
-                                        <Button className="create-new-job">Create New Job</Button>
-                                    </Link>
-                                    <Link to={`/projects/${this.projectId}/spiders/${this.spiderId}/cronjobs`}>
-                                        <Button className="create-new-job">Go to Cronjobs</Button>
-                                    </Link>
+                                    {pageTab === "overview" && (
+                                        <Content>
+                                            <Title level={4} className="text-center">
+                                                {name}
+                                            </Title>
+                                            <Row justify="center" className="spider-data">
+                                                <Space direction="vertical" size="large">
+                                                    <Text>
+                                                        <b>Spider ID:</b>&nbsp; {this.spiderId}
+                                                    </Text>
+                                                    <Text>
+                                                        <b>Project ID:</b>
+                                                        <Link to={`/projects/${this.projectId}`}>
+                                                            &nbsp; {this.projectId}
+                                                        </Link>
+                                                    </Text>
+                                                    <Table
+                                                        columns={this.columns}
+                                                        dataSource={jobs}
+                                                        pagination={false}
+                                                        size="middle"
+                                                    />
+                                                </Space>
+                                            </Row>
+                                            <Pagination
+                                                className="pagination"
+                                                defaultCurrent={1}
+                                                total={count}
+                                                current={current}
+                                                pageSize={this.PAGE_SIZE}
+                                                onChange={this.onPageChange}
+                                                showSizeChanger={false}
+                                            />
+                                            <Link
+                                                to={`/projects/${this.projectId}/spiders/${this.spiderId}/jobs/create`}
+                                            >
+                                                <Button className="create-new-job">Create New Job</Button>
+                                            </Link>
+                                            <Link to={`/projects/${this.projectId}/spiders/${this.spiderId}/cronjobs`}>
+                                                <Button className="create-new-job">Go to Cronjobs</Button>
+                                            </Link>
+                                        </Content>
+                                    )}
+                                    {pageTab === "settings" && <Content>Settings</Content>}
                                 </Content>
                             </Layout>
                         ) : (
