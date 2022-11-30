2dc7853950e4a884a49f1d03c68010e8fc46f2e9
JoseArmandoChavezQuijahuaman <100543415+JoseArmandoChavezQuijahuaman@users.noreply.github.com>
2022-11-30 06:56:22 -0500

    Bitmaker 2210 (#109)
    * Delete unnecessary file
    
    * Add development and production settings
    
    * Update gitignore
    
    * Fix header problem

--- estela-web/webpack.config.js
+++ estela-web/webpack.config.js
@@ -3,73 +3,75 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  output: {
-    publicPath: "http://localhost:3000/",
-  },
+module.exports = (env) => {
+    return {
+        entry: './src/index.ts',
+        devtool: 'inline-source-map',
+        output: {
+            publicPath: "http://localhost:3000/",
+        },
 
-  resolve: {
-    alias: {
-      DropdownComponent: path.resolve(__dirname, 'src/defaultComponents'),
-    },
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
-
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
         resolve: {
-          fullySpecified: false,
+            alias: {
+            DropdownComponent: path.resolve(__dirname, 'src/defaultComponents'),
+            },
+            extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.svg$/,
-        use: [
-        {
-          loader: 'babel-loader',
-        },
-        {
-          loader: '@svgr/webpack',
-          options: { babel: false, icon: true},
-        }
-      ],
-      },
-      { test: /\.json$/, type: 'json' }
-    ],
-  },
 
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      favicon: "./public/favicon.ico",
-    }),
-    new Dotenv(
-      {
-        path: '.env.local',
-      }
-    ),
-    new ESLintPlugin({
-        extensions: ["js", "jsx", "ts", "tsx"],
-      }),
-  ],
+        devServer: {
+            port: 3000,
+            historyApiFallback: true,
+        },
+
+        module: {
+            rules: [
+            {
+                test: /\.m?js/,
+                type: "javascript/auto",
+                resolve: {
+                fullySpecified: false,
+                },
+            },
+            {
+                test: /\.(css|s[ac]ss)$/i,
+                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+            },
+            {
+                test: /\.(ts|tsx|js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                {
+                loader: 'babel-loader',
+                },
+                {
+                loader: '@svgr/webpack',
+                options: { babel: false, icon: true},
+                }
+            ],
+            },
+            { test: /\.json$/, type: 'json' }
+            ],
+        },
+
+        plugins: [
+            new HtmlWebPackPlugin({
+            template: "./public/index.html",
+            favicon: "./public/favicon.ico",
+            }),
+            new Dotenv(
+            {
+                path: env.production ?'.env.production' : '.env.development',
+            }
+            ),
+            new ESLintPlugin({
+                extensions: ["js", "jsx", "ts", "tsx"],
+            }),
+        ],
+    };
 };
