a79e4103b25b8d70df6a5d0a16597c94833a35d3
saul <luckly083@gmail.com>
2023-01-02 18:37:07 -0500

    Add initial configuration for billing app
    Adding initial configuration for the bitmaker_billing app and linting was applied

--- estela-api/config/urls.py
+++ estela-api/config/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path("billing/", include("bitmaker_billing.urls")),
 ]
