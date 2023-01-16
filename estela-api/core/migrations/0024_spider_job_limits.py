b1a188824cc911d94df4c2a0c52bf6d4539d53b1
mgonnav <mateo@mgonnav.com>
2023-01-16 16:37:16 -0500

    Change migration name

--- estela-api/core/migrations/0024_spider_job_limits.py
+++ estela-api/core/migrations/0024_spider_job_limits.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0021_spidercronjob_deleted"),
+        ("core", "0023_auto_20230113_0354"),
     ]
 
     operations = [
