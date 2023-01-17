ecad5bf85b59bd885215373f0a24be5859957399
saul <luckly083@gmail.com>
2023-01-17 00:07:45 -0500

    Export components with cloud microfrontend for bitmaker billing module
    Exported another components by cloud microfrontend, notifications for card information on billing module, and the authentication service for making requests to estela-django-api bitmaker_billing app.

--- estela-web/webpack.mf.config.js
+++ estela-web/webpack.mf.config.js
@@ -1,15 +1,15 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const ESLintPlugin = require('eslint-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const ESLintPlugin = require("eslint-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
 
 module.exports = (env) => {
     return {
-        entry: './src/index.ts',
-        devtool: 'inline-source-map',
+        entry: "./src/index.ts",
+        devtool: "inline-source-map",
         output: {
-            publicPath: env.publicPath ?  `${env.publicPath}` : "http://localhost:3000/",
+            publicPath: env.publicPath ? `${env.publicPath}` : "http://localhost:3000/",
         },
 
         resolve: {
@@ -23,71 +23,72 @@ module.exports = (env) => {
 
         module: {
             rules: [
-            {
-                test: /\.m?js/,
-                type: "javascript/auto",
-                resolve: {
-                    fullySpecified: false,
-                },
-            },
-            {
-                test: /\.(css|s[ac]ss)$/i,
-                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
-            },
-            {
-                test: /\.(ts|tsx|js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-            {
-                test: /\.svg$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
+                {
+                    test: /\.m?js/,
+                    type: "javascript/auto",
+                    resolve: {
+                        fullySpecified: false,
                     },
-                    {
-                        loader: '@svgr/webpack',
-                        options: { babel: false, icon: true},
-                    }
-                ],
-            },
-            { test: /\.json$/, type: 'json' }
+                },
+                {
+                    test: /\.(css|s[ac]ss)$/i,
+                    use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+                },
+                {
+                    test: /\.(ts|tsx|js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                    },
+                },
+                {
+                    test: /\.svg$/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                        },
+                        {
+                            loader: "@svgr/webpack",
+                            options: { babel: false, icon: true },
+                        },
+                    ],
+                },
+                { test: /\.json$/, type: "json" },
             ],
         },
 
         plugins: [
             new ModuleFederationPlugin({
-            name: "estela_web",
-            filename: "remoteEntry.js",
-            remotes: {
-                ExternalDropdownComponent: `user_dropdown_module@${env.remoteURL}remoteEntry.js`,
-            },
-            exposes: {
-                "./header": "./src/shared/header/index.tsx",
-            },
-            shared: {
-                ...deps,
-                react: {
-                singleton: true,
-                requiredVersion: deps.react,
+                name: "estela_web",
+                filename: "remoteEntry.js",
+                remotes: {
+                    ExternalDropdownComponent: `user_dropdown_module@${env.remoteURL}remoteEntry.js`,
                 },
-                "react-dom": {
-                singleton: true,
-                requiredVersion: deps["react-dom"],
+                exposes: {
+                    "./header": "./src/shared/header/index.tsx",
+                    "./authService": "./src/services/auth.service.ts",
+                    "./constants": "./src/constants.ts",
+                    "./notifications": "./src/shared/notifications/index.tsx",
                 },
-            },
+                shared: {
+                    ...deps,
+                    react: {
+                        singleton: true,
+                        requiredVersion: deps.react,
+                    },
+                    "react-dom": {
+                        singleton: true,
+                        requiredVersion: deps["react-dom"],
+                    },
+                },
             }),
             new HtmlWebPackPlugin({
                 template: "./public/index.html",
                 favicon: "./public/favicon.ico",
-            }
-            ),
+            }),
             new Dotenv({
                 path: env.production ? ".env.production" : ".env.development",
-            }
-            ),
+            }),
             new ESLintPlugin({
                 extensions: ["js", "jsx", "ts", "tsx"],
             }),
