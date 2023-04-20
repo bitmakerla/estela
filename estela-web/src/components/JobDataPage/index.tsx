6501aa49215f0c8a0abc08b3b5008dbf63fd6c4c
Raymond Negron <raymond1242@Raymonds-MacBook-Air.local>
2023-04-20 09:36:50 -0500

    Adding JobSpecific data files

---
+++ estela-web/src/components/JobDataPage/index.tsx
@@ -0,0 +1,15 @@
+export const JobItemsData: React.FC = () => {
+    return <>Hello from Items</>;
+};
+
+export const JobRequestsData: React.FC = () => {
+    return <>Hello from Requests</>;
+};
+
+export const JobLogsData: React.FC = () => {
+    return <>Hello from Logs</>;
+};
+
+export const JobStatsData: React.FC = () => {
+    return <>Hello from Stats</>;
+};
