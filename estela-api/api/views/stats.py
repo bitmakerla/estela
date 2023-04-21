0629ba0f626add13137d2b3bd82144a50cca3a36
saul <luckly083@gmail.com>
2023-04-20 20:30:53 -0500

    Update Viewsets and adding Mixins
    Updated the viewsets for global stats and spider jobs viewsets, also updated the documentation and added a mixin for common functions in both stats viewset

--- estela-api/api/views/stats.py
+++ estela-api/api/views/stats.py
@@ -1,9 +1,8 @@
 from datetime import datetime
-from threading import Thread, Lock
-from multiprocessing import Process
 from typing import List
 
 from django.core.paginator import Paginator
+from re import findall
 from django.http.response import JsonResponse, HttpResponse
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
@@ -11,12 +10,18 @@ from rest_framework import status, viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.exceptions import NotFound, ParseError, PermissionDenied
 from rest_framework.response import Response
+from rest_framework.serializers import ListSerializer
 from rest_framework.utils.urls import replace_query_param
-from bson.json_util import dumps
+from json import dumps
 
 from api import errors
 from api.exceptions import DataBaseError
 from api.mixins import BaseViewSet
+from api.serializers.stats import (
+    StatsSerializer,
+    GlobalStatsSerializer,
+    SpidersJobsStatsSerializer,
+)
 
 from config.job_manager import spiderdata_db_client
 
@@ -27,27 +32,167 @@ from core.models import (
 )
 
 
-# class StatsViewSet(BaseViewSet, viewsets.ModelViewSet):
-class OverallStatsViewSet(BaseViewSet, mixins.ListModelMixin):
+class StatsForDashboardMixin:
+    def get_parameters(self, request):
+        start_date = request.query_params.get("start_date", datetime.today())
+        end_date = request.query_params.get("end_date", datetime.today())
+        return start_date, end_date
+
+    def summarize_stats_results(
+        self, stats_set: List[dict], jobs_set: SpiderJob
+    ) -> dict:
+        global_stats_results = {}
+        jobs_ids = {}
+        for job in jobs_set:
+            date_str = job.created.strftime("%Y-%m-%d")
+            jobs_ids[job.jid] = date_str
+            if date_str not in global_stats_results:
+                global_stats_results[date_str] = {
+                    "jobs": {
+                        "total_jobs": 1,
+                        "error_jobs": int(job.status == SpiderJob.ERROR_STATUS),
+                        "unknown_jobs": int(
+                            job.status != SpiderJob.ERROR_STATUS
+                            and job.status != SpiderJob.RUNNING_STATUS
+                            and job.status != SpiderJob.COMPLETED_STATUS
+                        ),
+                        "running_jobs": int(job.status == SpiderJob.RUNNING_STATUS),
+                        "finished_jobs": int(job.status == SpiderJob.COMPLETED_STATUS),
+                    },
+                    "pages": {
+                        "total_pages": 0,
+                        "scraped_pages": 0,
+                        "missed_pages": 0,
+                    },
+                    "items_count": 0,
+                    "runtime": 0.0,
+                    "status_codes": {
+                        "status_200": 0,
+                        "status_301": 0,
+                        "status_302": 0,
+                        "status_401": 0,
+                        "status_403": 0,
+                        "status_404": 0,
+                        "status_429": 0,
+                        "status_500": 0,
+                    },
+                    "success_rate": 0.0,
+                    "logs": {
+                        "total_logs": 0,
+                        "debug_logs": 0,
+                        "info_logs": 0,
+                        "warning_logs": 0,
+                        "error_logs": 0,
+                        "critical_logs": 0,
+                    },
+                }
+            else:
+                global_stats_results[date_str]["jobs"]["total_jobs"] += 1
+                global_stats_results[date_str]["jobs"]["running_jobs"] += int(
+                    job.status == SpiderJob.RUNNING_STATUS
+                )
+                global_stats_results[date_str]["jobs"]["unknown_jobs"] += int(
+                    job.status != SpiderJob.ERROR_STATUS
+                    and job.status != SpiderJob.RUNNING_STATUS
+                    and job.status != SpiderJob.COMPLETED_STATUS
+                )
+                global_stats_results[date_str]["jobs"]["error_jobs"] += int(
+                    job.status == SpiderJob.ERROR_STATUS
+                )
+                global_stats_results[date_str]["jobs"]["finished_jobs"] += int(
+                    job.status == SpiderJob.COMPLETED_STATUS
+                )
+
+        for stats in stats_set:
+            job_id = int(findall(r"\d+", stats["_id"])[1])
+            date_str = jobs_ids[job_id]
+            global_stats_results[date_str]["items_count"] += stats.get(
+                "item_scraped_count", 0
+            )
+            global_stats_results[date_str]["runtime"] += stats.get(
+                "elapsed_time_seconds", 0.0
+            )
+            downloader_request_count = stats.get("downloader/request_count", 0.0)
+            global_stats_results[date_str]["success_rate"] += (
+                100
+                * stats.get("response_received_count", 0.0)
+                / downloader_request_count
+                if downloader_request_count != 0.0
+                else 0.0
+            )
+            global_stats_results[date_str]["runtime"] += stats.get(
+                "elapsed_time_seconds", 0.0
+            )
+
+            global_stats_results[date_str]["pages"]["scraped_pages"] += stats.get(
+                "downloader/response_status_count/200", 0
+            )
+            global_stats_results[date_str]["pages"]["missed_pages"] += stats.get(
+                "response_received_count", 0
+            ) - stats.get("downloader/response_status_count/200", 0)
+            global_stats_results[date_str]["pages"]["total_pages"] += stats.get(
+                "response_received_count", 0
+            )
+
+            global_stats_results[date_str]["status_codes"]["status_200"] += stats.get(
+                "downloader/response_status_count/200", 0
+            )
+            global_stats_results[date_str]["status_codes"]["status_301"] += stats.get(
+                "downloader/response_status_count/301", 0
+            )
+            global_stats_results[date_str]["status_codes"]["status_302"] += stats.get(
+                "downloader/response_status_count/302", 0
+            )
+            global_stats_results[date_str]["status_codes"]["status_401"] += stats.get(
+                "downloader/response_status_count/401", 0
+            )
+            global_stats_results[date_str]["status_codes"]["status_403"] += stats.get(
+                "downloader/response_status_count/403", 0
+            )
+            global_stats_results[date_str]["status_codes"]["status_404"] += stats.get(
+                "downloader/response_status_count/404", 0
+            )
+            global_stats_results[date_str]["status_codes"]["status_429"] += stats.get(
+                "downloader/response_status_count/429", 0
+            )
+            global_stats_results[date_str]["status_codes"]["status_500"] += stats.get(
+                "downloader/response_status_count/500", 0
+            )
+
+            global_stats_results[date_str]["logs"]["debug_logs"] += stats.get(
+                "log_count/DEBUG", 0
+            )
+            global_stats_results[date_str]["logs"]["info_logs"] += stats.get(
+                "log_count/INFO", 0
+            )
+            global_stats_results[date_str]["logs"]["warning_logs"] += stats.get(
+                "log_count/WARNING", 0
+            )
+            global_stats_results[date_str]["logs"]["error_logs"] += stats.get(
+                "log_count/ERROR", 0
+            )
+            global_stats_results[date_str]["logs"]["critical_logs"] += stats.get(
+                "log_count/CRITICAL", 0
+            )
+            global_stats_results[date_str]["logs"]["total_logs"] += (
+                global_stats_results[date_str]["logs"]["debug_logs"]
+                + global_stats_results[date_str]["logs"]["info_logs"]
+                + global_stats_results[date_str]["logs"]["warning_logs"]
+                + global_stats_results[date_str]["logs"]["error_logs"]
+                + global_stats_results[date_str]["logs"]["critical_logs"]
+            )
+            # Here we need to add the coverage also
+
+        return global_stats_results
+
+
+class OverallStatsViewSet(BaseViewSet, StatsForDashboardMixin, mixins.ListModelMixin):
     model_class = Project
     lookup_field = "pid"
 
     MAX_PAGINATION_SIZE = 100
     MIN_PAGINATION_SIZE = 1
     DEFAULT_PAGINATION_SIZE = 50
-
-    def get_parameters(self, request):
-        page = int(request.query_params.get("page", 1))
-        page_size = int(
-            request.query_params.get("page_size", self.DEFAULT_PAGINATION_SIZE)
-        )
-        return page, page_size
-
-    def get_paginated_link(self, page_number):
-        if page_number < 1:
-            return None
-        url = self.request.build_absolute_uri()
-        return replace_query_param(url, "page", page_number)
 
     @swagger_auto_schema(
         operation_description="Retrieve stats of all jobs in a range of time, dates must have the format YYYY-mm-dd.",
@@ -67,12 +212,15 @@ class OverallStatsViewSet(BaseViewSet, mixins.ListModelMixin):
                 description="End of date range (e.g. 2023-04-30).",
             ),
         ],
+        responses={
+            status.HTTP_200_OK: openapi.Response(
+                description="Global Stats operation success",
+                schema=ListSerializer(child=GlobalStatsSerializer()),
+            ),
+        },
     )
     def list(self, request, *args, **kwargs):
-        start_date = request.query_params.get(
-            "start_date", datetime.today().replace(day=1)
-        )
-        end_date = request.query_params.get("end_date", datetime.today().replace(day=1))
+        start_date, end_date = self.get_parameters(request)
         if not spiderdata_db_client.get_connection():
             raise DataBaseError({"error": errors.UNABLE_CONNECT_DB})
 
@@ -81,84 +229,40 @@ class OverallStatsViewSet(BaseViewSet, mixins.ListModelMixin):
         jobs_set = SpiderJob.objects.filter(
             spider__in=sid_set, created__range=[start_date, end_date]
         )
-        # For coverage is there a function mon mongodb to count the ocurrences of each field
-        # for an object in a collection?
-        result = {}
-        lock = Lock()
-        def get_collection_data(collection_name, date: str):
-            doc = spiderdata_db_client.get_all_collection_data(
-                kwargs["pid"], collection_name
-            )
-            with lock:
-                if result.get(date) is not None:
-                    result[date].append(doc)
-                else:
-                    result[date] = [doc]
 
-        threads:List[Thread] = []
-        for job in jobs_set:
-            job_collection_name = ""
-            # No considerar este caso con cronjob
-            if job.cronjob is not None and job.cronjob.unique_collection:
-                job_collection_name = "{}-scj{}-job_stats".format(
-                    job.spider.sid, job.cronjob.cjid
-                )
-            else:
-                job_collection_name = "{}-{}-job_stats".format(job.spider.sid, job.jid)
-            
-            t = Thread(
-                target=get_collection_data, 
-                args=(job_collection_name, job.created.strftime("%Y-%m-%d"),)
-            )
-            threads.append(t)
-            t.start()
+        job_collections_names: List[str] = [
+            "{}-{}-job_stats".format(job.spider.sid, job.jid) for job in jobs_set
+        ]
 
-        for t in threads:
-            t.join()
+        stats_set: List[dict] = spiderdata_db_client.get_jobs_set_stats(
+            kwargs["pid"], job_collections_names
+        )
 
-        # Error race condition
-        # processes:List[Process] = []
-        # for job in jobs_set:
-        #     job_collection_name = ""
-        #     if job.cronjob is not None and job.cronjob.unique_collection:
-        #         job_collection_name = "{}-scj{}-job_stats".format(
-        #             job.spider.sid, job.cronjob.cjid
-        #         )
-        #     else:
-        #         job_collection_name = "{}-{}-job_stats".format(job.spider.sid, job.jid)
-            
-        #     p = Process(
-        #         target=get_collection_data, 
-        #         args=(job_collection_name,)
-        #     )
-        #     processes.append(p)
-        #     p.start()
+        global_stats_results = self.summarize_stats_results(stats_set, jobs_set)
 
-        # for p in processes:
-        #     p.join()
-        
-        return JsonResponse(result, safe=False)
+        response_schema = []
+        for date, stat_result in global_stats_results.items():
+            # Averaging some stat variables and adding values to the response schema
+            stat_result["success_rate"] /= stat_result["jobs"]["finished_jobs"]
+            stat_serializer = StatsSerializer(data=stat_result)
+            if stat_serializer.is_valid():
+                response_schema.append({"date": date, "stats": stat_serializer.data})
 
-class SpidersJobsStatsViewSet(BaseViewSet, mixins.ListModelMixin):
+        return Response(
+            data=response_schema,
+            status=status.HTTP_200_OK,
+        )
+
+
+class SpidersJobsStatsViewSet(
+    BaseViewSet, StatsForDashboardMixin, mixins.ListModelMixin
+):
     model_class = Spider
     lookup_field = "sid"
 
     MAX_PAGINATION_SIZE = 100
     MIN_PAGINATION_SIZE = 1
     DEFAULT_PAGINATION_SIZE = 50
-
-    def get_parameters(self, request):
-        page = int(request.query_params.get("page", 1))
-        page_size = int(
-            request.query_params.get("page_size", self.DEFAULT_PAGINATION_SIZE)
-        )
-        return page, page_size
-
-    def get_paginated_link(self, page_number):
-        if page_number < 1:
-            return None
-        url = self.request.build_absolute_uri()
-        return replace_query_param(url, "page", page_number)
 
     @swagger_auto_schema(
         operation_description="Retrieve stats of all jobs of a spider in a range of time, dates must have the format YYYY-mm-dd.",
@@ -178,48 +282,42 @@ class SpidersJobsStatsViewSet(BaseViewSet, mixins.ListModelMixin):
                 description="End of date range (e.g. 2023-04-30).",
             ),
         ],
+        responses={
+            status.HTTP_200_OK: openapi.Response(
+                description="Spiders/Jobs Stats operation success",
+                schema=ListSerializer(child=SpidersJobsStatsSerializer()),
+            ),
+        },
     )
     def list(self, request, *args, **kwargs):
-        start_date = request.query_params.get(
-            "start_date", datetime.today().replace(day=1)
-        )
-        end_date = request.query_params.get("end_date", datetime.today().replace(day=1))
+        start_date, end_date = self.get_parameters(request)
+
         if not spiderdata_db_client.get_connection():
             raise DataBaseError({"error": errors.UNABLE_CONNECT_DB})
 
-        
         jobs_set = SpiderJob.objects.filter(
             spider__in=kwargs["sid"], created__range=[start_date, end_date]
         )
 
-        result = []
-        lock = Lock()
-        def get_collection_data(collection_name):
-            doc = spiderdata_db_client.get_all_collection_data(
-                kwargs["pid"], collection_name
-            )
-            with lock:
-                result.extend(doc)
+        job_collections_names: List[str] = [
+            "{}-{}-job_stats".format(job.spider.sid, job.jid) for job in jobs_set
+        ]
 
-        threads:List[Thread] = []
-        for job in jobs_set:
-            job_collection_name = ""
-            if job.cronjob is not None and job.cronjob.unique_collection:
-                job_collection_name = "{}-scj{}-job_stats".format(
-                    job.spider.sid, job.cronjob.cjid
-                )
-            else:
-                job_collection_name = "{}-{}-job_stats".format(job.spider.sid, job.jid)
-            
-            t = Thread(
-                target=get_collection_data, 
-                args=(job_collection_name,)
-            )
-            threads.append(t)
-            t.start()
+        stats_set: List[dict] = spiderdata_db_client.get_jobs_set_stats(
+            kwargs["pid"], job_collections_names
+        )
 
-        for t in threads:
-            t.join()
-        
-        print(dumps(result))
-        return Response(status=status.HTTP_200_OK)
+        global_stats_results = self.summarize_stats_results(stats_set, jobs_set)
+
+        response_schema = []
+        for date, stat_result in global_stats_results.items():
+            # Averaging some stat variables and adding values to the response schema
+            stat_result["success_rate"] /= stat_result["jobs"]["finished_jobs"]
+            stat_serializer = StatsSerializer(data=stat_result)
+            if stat_serializer.is_valid():
+                response_schema.append({"date": date, "stats": stat_serializer.data})
+
+        return Response(
+            data=response_schema,
+            status=status.HTTP_200_OK,
+        )
