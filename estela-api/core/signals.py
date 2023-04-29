1616f6193a0a9e66f305d75f61d4e63448f1f96d
saul <luckly083@gmail.com>
2023-04-29 13:27:21 -0500

    Update tasks.py to calculate coverage
    Coverage is calculated and saved on the job_stats document after it finish its execution

--- estela-api/core/signals.py
+++ estela-api/core/signals.py
@@ -2,14 +2,18 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from core.tasks import get_chain_to_process_usage_data
+from core.tasks import get_chain_to_process_usage_data, record_job_coverage_event
 from core.models import SpiderJob
 
 
 @receiver(post_save, sender=SpiderJob, dispatch_uid="update_usage")
-def update_usage(sender, instance, created, **kwargs):
+def update_usage(sender, instance: SpiderJob, created, **kwargs):
     if instance.status == SpiderJob.COMPLETED_STATUS:
         chain_of_usage_process = get_chain_to_process_usage_data(job_id=instance.jid)
         chain_of_usage_process.apply_async(
             countdown=settings.COUNTDOWN_RECORD_PROJECT_USAGE_AFTER_JOB_EVENT
+        )
+        record_job_coverage_event.apply_async(
+            args=[instance.jid],
+            countdown=settings.COUNTDOWN_RECORD_COVERAGE_AFTER_JOB_EVENT,
         )
