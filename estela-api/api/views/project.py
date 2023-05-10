810d657cf2daf2f8a387a6b9f8a7ae64e077258f
Raymond Negron <raymond1242@Raymonds-MacBook-Air.local>
2023-05-10 08:38:17 -0500

    Handling exceptional cases updating envvars

--- estela-api/api/views/project.py
+++ estela-api/api/views/project.py
@@ -32,20 +32,22 @@ from core.models import (
 
 
 def update_env_vars(instance, env_vars, level="project"):
-    env_vars_project = instance.env_vars.all()
+    env_vars_instance = instance.env_vars.all()
     for env_var in env_vars:
-        if env_vars_project.filter(**env_var).exists():
+        if env_vars_instance.filter(**env_var).exists():
             continue
         elif env_var["masked"] is True and env_var["value"] == "__MASKED__":
             continue
-        elif env_var["name"] in [value.name for value in env_vars_project]:
-            env_vars_project.filter(name=env_var["name"]).update(value=env_var["value"])
+        elif env_var["masked"] is False and env_var["value"] == "__MASKED__":
+            env_vars_instance.filter(name=env_var["name"]).update(masked=False)
+        elif env_var["name"] in [value.name for value in env_vars_instance]:
+            env_vars_instance.filter(name=env_var["name"]).update(value=env_var["value"], masked=env_var["masked"])
         else:
             if level == "project":
                 SpiderJobEnvVar.objects.create(project=instance, **env_var)
             elif level == "spider":
                 SpiderJobEnvVar.objects.create(spider=instance, **env_var)
-    for env_var in env_vars_project:
+    for env_var in env_vars_instance:
         if env_var.name not in [value["name"] for value in env_vars]:
             env_var.delete()
 
