f3b8b77d63121b9a05bc423d635c42a84ada4aee
emegona <mateo@emegona.com>
2023-06-11 23:36:58 -0500

    Update migrations and field descriptions

--- estela-api/core/models.py
+++ estela-api/core/models.py
@@ -419,18 +419,20 @@ class Notification(models.Model):
     )
     message = models.CharField(max_length=1000, help_text="Notification message.")
     project = models.ForeignKey(
-        Project, on_delete=models.CASCADE, help_text="Project belong the notification"
+        Project,
+        on_delete=models.CASCADE,
+        help_text="Project where the notification belongs",
     )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        help_text="User who created the notification.",
+        help_text="User who performed the action on this notification.",
     )
     users = models.ManyToManyField(
         User,
         through="UserNotification",
         related_name="notifications",
-        help_text="Users subscribed to this notification.",
+        help_text="Users that received this notification.",
     )
 
 
@@ -438,19 +440,21 @@ class UserNotification(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        help_text="User whose this notification belongs to.",
+        help_text="Related user.",
     )
     notification = models.ForeignKey(
         Notification,
         on_delete=models.CASCADE,
-        help_text="Notification to which the user is subscribed.",
+        help_text="Notification that the user received.",
     )
     seen = models.BooleanField(
         default=False, help_text="Whether the notification was seen."
     )
-    created_at = models.DateTimeField(
-        auto_now_add=True, editable=False, help_text="Subscription date."
+    created = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+        help_text="Date when the notification was sent.",
     )
 
     class Meta:
-        ordering = ["-created_at"]
+        ordering = ["-created"]
