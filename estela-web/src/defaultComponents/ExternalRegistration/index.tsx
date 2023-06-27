79ef8d75b5853eaebba3be3e0e7e90a253cf0e39
Joel Perca <joelperca.vc@gmail.com>
2023-06-26 20:25:19 -0500

    Adding external registration components

---
+++ estela-web/src/defaultComponents/ExternalRegistration/index.tsx
@@ -0,0 +1,4 @@
+export { CheckBoxRegistration } from "./components/CheckBox";
+export { RegistrationProvider } from "./context/RegistrationProvider";
+export { RegistrationContext } from "./context/RegistrationContext";
+export type { RegistrationContextProps } from "./context/RegistrationContext";
