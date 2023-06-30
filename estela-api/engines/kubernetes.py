969a97ac135f7667ad71727d848177420efed22d
Joaquin <joaquingc123@gmail.com>
2023-06-30 00:27:06 -0500

    limit pod privileges.

--- estela-api/engines/kubernetes.py
+++ estela-api/engines/kubernetes.py
@@ -68,26 +68,56 @@ class KubernetesEngine:
                 name=volume_spec["name"],
                 host_path=host_path,
             )
-        container = client.V1Container(
-            name=container_name,
-            image=container_image,
-            env=env_list,
-            command=command,
-            image_pull_policy=self.IMAGE_PULL_POLICY,
-            volume_mounts=[volume_mount] if volume_mount else None,
-        )
-
-        template.template.spec = client.V1PodSpec(
-            containers=[container],
-            restart_policy=self.POD_RESTART_POLICY,
-            image_pull_secrets=[
-                client.V1LocalObjectReference(self.IMAGE_PULL_SECRET_NAME)
-            ],
-            volumes=[volume] if volume else None,
-            node_selector=(
-                {"role": self.SPIDER_NODE_ROLE} if settings.MULTI_NODE_MODE else None
-            ),
-        )
+        if isbuild:
+            container = client.V1Container(
+                name=container_name,
+                image=container_image,
+                env=env_list,
+                command=command,
+                image_pull_policy=self.IMAGE_PULL_POLICY,
+                volume_mounts=[volume_mount] if volume_mount else None,
+            )
+        else: 
+            container = client.V1Container(
+                name=container_name,
+                image=container_image,
+                env=env_list,
+                command=command,
+                image_pull_policy=self.IMAGE_PULL_POLICY,
+                volume_mounts=[volume_mount] if volume_mount else None,
+                security_context=client.V1SecurityContext(
+                    capabilities=client.V1Capabilities(drop=["ALL"])
+                )
+            )
+            
+        if isbuild:
+            template.template.spec = client.V1PodSpec(
+                containers=[container],
+                restart_policy=self.POD_RESTART_POLICY,
+                image_pull_secrets=[
+                    client.V1LocalObjectReference(self.IMAGE_PULL_SECRET_NAME)
+                ],
+                volumes=[volume] if volume else None,
+                node_selector=(
+                    {"role": self.SPIDER_NODE_ROLE} if settings.MULTI_NODE_MODE else None
+                ),
+            )
+        else:
+            template.template.spec = client.V1PodSpec(
+                containers=[container],
+                security_context=client.V1PodSecurityContext(
+                    run_as_non_root=True,
+                    run_as_user=1000
+                ),
+                restart_policy=self.POD_RESTART_POLICY,
+                image_pull_secrets=[
+                    client.V1LocalObjectReference(self.IMAGE_PULL_SECRET_NAME)
+                ],
+                volumes=[volume] if volume else None,
+                node_selector=(
+                    {"role": self.SPIDER_NODE_ROLE} if settings.MULTI_NODE_MODE else None
+                ),
+            )
 
         body.spec = client.V1JobSpec(
             ttl_seconds_after_finished=self.JOB_TTL_SECONDS_AFTER_FINISHED,
