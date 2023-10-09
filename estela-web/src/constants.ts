48c1ef8d774c1d06afae0353f118a7bfcc9c010f
Joaquin <joaquingc123@gmail.com>
2023-10-09 13:04:09 -0500

    Add migrations. Add env var.

--- estela-web/src/constants.ts
+++ estela-web/src/constants.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-export const ESTELA_PROXIES = process.env.ESTELA_PROXIES;
+export const ESTELA_PROXIES = process.env.ESTELA_PROXIES ? process.env.ESTELA_PROXIES : "Estela Proxy";
 export const REGISTER_PAGE_ENABLED = process.env.REGISTER_PAGE_ENABLED === "true";
